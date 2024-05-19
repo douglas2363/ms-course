508e3a66d88954c8e0615502eb254bb2516b4e2d
Douglas <douglaslima2363@gmail.com>
2024-05-19 12:05:12 -0300

    Configuração dos microserviços no api gateway

--- hr-api-gateway-zull/src/main/java/com/devsuperior/hrapigatewayzull/HrApiGatewayZullApplication.java
+++ hr-api-gateway-zull/src/main/java/com/devsuperior/hrapigatewayzull/HrApiGatewayZullApplication.java
@@ -2,7 +2,11 @@ package com.devsuperior.hrapigatewayzull;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableEurekaClient // Habilita o Eureka
+@EnableZuulProxy // Habilita o Zull
 @SpringBootApplication
 public class HrApiGatewayZullApplication {
 
