7301cb580d42e3b206bf849b20b2f7f93f1f498a
Douglas <douglaslima2363@gmail.com>
2024-07-07 10:45:49 -0300

    FeignClient

--- hr-oauth/src/main/java/com/devsuperior/hr_oauth/HrOauthApplication.java
+++ hr-oauth/src/main/java/com/devsuperior/hr_oauth/HrOauthApplication.java
@@ -3,9 +3,12 @@ package com.devsuperior.hr_oauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+
+@EnableFeignClients(basePackages = "com.devsuperior.hr_oauth.feignclients")
 @EnableEurekaClient
+@SpringBootApplication
 public class HrOauthApplication {
 
 	public static void main(String[] args) {
